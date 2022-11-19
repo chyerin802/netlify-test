d44dbefef729a645f0d49eb6f3bcc0b8edc90e74
chyerin802 <chyerin802@gmail.com>
2022-11-19 14:03:21 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [hi, setHi] = useState('');
+  useEffect(()=>{
+    const getHello = async () =>{
+      try{
+        const res = await axios("http://127.0.0.1:5000/hello")
+        console.log(res.data)
+        setHi(res.data)
+      }catch(e){
+        console.log(e.response.data)
+      }
+    }
+
+    getHello()
+  }, [])
+
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {hi}
         </p>
         <a
           className="App-link"
